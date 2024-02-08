fce49432da082375fe7b400ad43e120650746c85
Admin <dankom2007@gmail.com>
2024-02-08 12:39:10 +0300

    test1

--- 2.py
+++ 2.py
@@ -1,10 +1,7 @@
-from itertools import product
-
-count = 0
-for i in product('012345678', repeat=5):
-    if i.count('5') == 1 and '00' not in i and '11' not in i and '22' not in i and '33' not in i and '44' not in i and '55' not in i and '66' not in i and '77' not in i and '88' not in i:
-        pass
-    if i.count('5') == 1 and i[0] != i[1] and i[1] != i[2] and i[2] != i[3] and i[3] != i[4] and i[0] != '0':
-        count += 1
-
-print(count)
+print('x', 'y', 'z', 'w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if ((x or (not y)) and (y == z) and w):
+                    print(x, y, z, w)
